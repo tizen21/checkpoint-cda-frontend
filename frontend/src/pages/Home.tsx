588330414e451c3a9e09e277f3c2d282f93fef6a
Antho <rey.anthony1@gmail.com>
2025-05-12 10:08:56 +0200

    Initialization & tailwind installation

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold underline">Hello you!</h1>
+    </div>
+  );
+}
